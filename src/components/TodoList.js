d7071b97f7050fc60385a4b1a133ba010e50020c
David Aouizerats <david.aouizerats@gmail.com>
2021-03-09 10:06:01 +0200

    remvove editing hook

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,15 +1,11 @@
-import { useEditing } from "../hooks/useEditing";
 import TodoListItem from "./TodoListItem";
 
 function TodoList({ todos }) {
-
-    const editing = useEditing();
-
     return (
         <section className="main">
             <input id="toggle-all" className="toggle-all" type="checkbox" onChange={todos.toggleAll} checked={todos.collection.every(t => t.completed)} />
             <ul className="todo-list">
-                {todos.collection.map(t => <TodoListItem key={t.id} editing={editing} todo={t} removeTodo={todos.removeItem} />)}
+                {todos.collection.map(t => <TodoListItem key={t.id} todo={t} removeTodo={todos.removeItem} />)}
             </ul>
         </section>
     );
--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -1,24 +1,30 @@
 import classNames from "classnames";
 import { useEffect, useRef, useState } from 'react'
 
-function TodoListItem({ todo, removeTodo, editing }) {
+function TodoListItem({ todo, removeTodo }) {
 
     const [reload, setReload] = useState(false);
+    const [edit, setEdit] = useState({ editing: false, editText: "" });
     const editor = useRef();
 
     useEffect(() => {
-        if (editor.current) {
-            editor.current.focus();
-            editor.current.setSelectionRange(0, editor.current.value.length)
+        if (edit.editing) {
+            setEdit({ ...edit, editText: todo.title });
+
+            if (editor.current) {
+                editor.current.focus();
+                editor.current.setSelectionRange(0, editor.current.value.length)
+            }
         }
 
-    }, [editing.isEditing(todo)])
+    }, [edit.editing])
 
     function handleSubmit(e) {
-        var val = editing.editText.trim();
+        var val = edit.editText.trim();
         if (val) {
-            editing.endEdit(todo, val);
-        } else if (editing.isEditing(todo)) {
+            todo.title = val;
+            setEdit({ ...edit, editing: false, editText: "" });
+        } else if (edit.editing) {
             removeTodo(todo);
         }
     }
@@ -29,7 +35,7 @@ function TodoListItem({ todo, removeTodo, editing }) {
         }
 
         if (e.key === 'Escape') {
-            editing.cancelEdit();
+            setEdit({ ...edit, editing: false, editText: "" });
         }
     }
 
@@ -41,19 +47,19 @@ function TodoListItem({ todo, removeTodo, editing }) {
     return (
         <li className={classNames({
             completed: todo.completed,
-            editing: editing.isEditing(todo),
+            editing: edit.editing,
         })}>
             <div className="view">
                 <input key={todo} className="toggle" type="checkbox" checked={todo.completed} onChange={handleToggle} />
-                <label onDoubleClick={() => editing.startEdit(todo)}>{todo.title}</label>
+                <label onDoubleClick={() => setEdit({ ...edit, editing: true, editText: todo.title })}>{todo.title}</label>
                 <button className="destroy" onClick={() => removeTodo(todo)} />
             </div>
             <input className="edit"
                 ref={editor}
-                value={editing.editText}
+                value={edit.editText}
                 onKeyUp={handleKeyUp}
                 onBlur={handleSubmit}
-                onChange={e => editing.updateEdit(todo, e.target.value)} />
+                onChange={e => setEdit({ ...edit, editText: e.target.value })} />
         </li>
     );
 }
